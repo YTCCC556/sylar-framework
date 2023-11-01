c808a7ebea35d0f79ae9ba1a70924569da807416
YTCCC556 <2276261257@qq.com>
2023-11-01 11:53:42 +0800

    支持自定义格式容器的解析

--- include/config.h
+++ include/config.h
@@ -35,6 +35,7 @@ public:
 
     virtual std::string toString() = 0;
     virtual bool fromString(const std::string &val) = 0;
+    virtual std::string getTypeName() const = 0;
 
 private:
     std::string m_name;
@@ -262,7 +263,7 @@ public:
         }
         return false;
     }
-
+    std::string getTypeName() const override { return typeid(T).name(); }
     const T getValue() const { return m_val; }
     void setValue(const T &v) { m_val = v; }
 
@@ -278,12 +279,24 @@ public:
     static typename ConfigVar<T>::ptr
     Lookup(const std::string &name, const T &default_value,
            const std::string &description = "") {
-        auto tmp = Lookup<T>(name);
-        if (tmp) {
-            SYLAR_LOG_INFO(SYLAR_LOG_ROOT())
-                    << "Lookup name = " << name << "exists";
-            return tmp;
+        //返回为空，两种情况，真不存在，或者存在但是类型不同
+        auto it = m_datas.find(name);
+        if (it != m_datas.end()) {
+            auto tmp = std::dynamic_pointer_cast<ConfigVar<T>>(it->second);
+            if (tmp) {
+                SYLAR_LOG_INFO(SYLAR_LOG_ROOT())
+                        << "Lookup name = " << name << " exists";
+                return tmp;
+            } else {
+                SYLAR_LOG_ERROR(SYLAR_LOG_ROOT())
+                        << "Lookup name = " << name << " exists but type not "
+                        << typeid(T).name()
+                        << "real_type = " << it->second->getTypeName() << ""
+                        << it->second->toString();
+                return nullptr;
+            }
         }
+
         if (name.find_first_not_of("qwertyuiopasdfghjklzxcvbnm"
                                    "._0123456789") != std::string::npos) {
             SYLAR_LOG_ERROR(SYLAR_LOG_ROOT()) << "Lookup name invall" << name;
@@ -308,6 +321,7 @@ public:
 private:
     static ConfigVarMap m_datas;
 };
+
 
 
 }// namespace ytccc
--- tests/tests_config.cpp
+++ tests/tests_config.cpp
@@ -113,6 +113,86 @@ void test_config() {
     XX_M(g_unordermap_value_config, unordermap, after);
 #undef XX
 }
+class Person {
+public:
+    std::string m_name;
+    int m_age = 0;
+    bool m_sex;
+
+    std::string toString() const {
+        std::stringstream ss;
+        ss << "[Person name=" << m_name << " age=" << m_age << " sex=" << m_sex
+           << "]";
+        return ss.str();
+    }
+};
+namespace ytccc {
+//自定义偏特化
+template<>
+class LexicalCast<std::string, Person> {
+public:
+    Person operator()(const std::string &v) {
+        YAML::Node node = YAML::Load(v);
+        Person p;
+        p.m_name = node["name"].as<std::string>();
+        p.m_age = node["age"].as<int>();
+        p.m_sex = node["sex"].as<bool>();
+        return p;
+    }
+};
+template<>
+class LexicalCast<Person, std::string> {
+public:
+    std::string operator()(const Person &p) {
+        YAML::Node node;
+        node["name"] = p.m_name;
+        node["age"] = p.m_age;
+        node["sex"] = p.m_sex;
+        std::stringstream ss;
+        ss << node;
+        return ss.str();
+    }
+};
+
+}// namespace ytccc
+
+ytccc::ConfigVar<Person>::ptr g_person =
+        ytccc::Config::Lookup("class.person", Person(), "system person");
+ytccc::ConfigVar<std::map<std::string, Person>>::ptr g_person_map =
+        ytccc::Config::Lookup("class.map", std::map<std::string, Person>(),
+                              "system map person");
+ytccc::ConfigVar<std::map<std::string, std::vector<Person>>>::ptr
+        g_person_vec_map = ytccc::Config::Lookup(
+                "class.vec_map", std::map<std::string, std::vector<Person>>(),
+                "system map vec person");
+
+void test_class() {
+    SYLAR_LOG_INFO(SYLAR_LOG_ROOT())
+            << "before" << g_person->getValue().toString() << "-"
+            << g_person->toString();
+#define XX_PM(g_var, prefix)                                                   \
+    {                                                                          \
+        auto m = g_person_map->getValue();                                     \
+        for (auto &i: m) {                                                     \
+            SYLAR_LOG_INFO(SYLAR_LOG_ROOT())                                   \
+                    << #prefix << i.first << "-" << i.second.toString();       \
+        }                                                                      \
+        SYLAR_LOG_INFO(SYLAR_LOG_ROOT())                                       \
+                << #prefix << " : size = " << m.size();                        \
+    }
+
+    XX_PM(g_person_map, before);
+    SYLAR_LOG_INFO(SYLAR_LOG_ROOT())
+            << "before vec map " << g_person_vec_map->toString();
+    YAML::Node root = YAML::LoadFile("../bin/conf/log.yml");
+    ytccc::Config::LoadFromYaml(root);
+    SYLAR_LOG_INFO(SYLAR_LOG_ROOT())
+            << "after" << g_person->getValue().toString() << "-"
+            << g_person->toString();
+    XX_PM(g_person_map, after);
+    SYLAR_LOG_INFO(SYLAR_LOG_ROOT())
+            << "after vec map " << g_person_vec_map->toString();
+}
 
 int main(int argc, char **argv) {
 
@@ -120,7 +200,8 @@ int main(int argc, char **argv) {
     //    SYLAR_LOG_INFO(SYLAR_LOG_ROOT()) << g_int_value_config->toString();
 
     //    test_yaml();
-    test_config();
+    //    test_config();
+    test_class();
 
     return 0;
 }
